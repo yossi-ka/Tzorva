7fec4b782c15aabc5d80a6b3ce046417632fbc3a
yossi-ka <yk111067@gmail.com>
2024-11-04 22:10:11 +0200

    firebase functions - interventions 1

--- tzorva-functions/index.js
+++ tzorva-functions/index.js
@@ -159,7 +159,7 @@ export const getStudents = functions.https.onRequest((req, res) => {
           success: true,
           massage: arr,
         });
-      } else if (userData.job_title === 'מנהל ת"ת') {
+      } else if (userData.job_title === "מנהל ת\"ת") {
         const city = userData.city;
 
         const q = db.collection("students").where("city_of_school", "==", city);
@@ -354,18 +354,115 @@ export const getArchive = functions.https.onRequest((req, res) => {
   });
 });
 
-// פונקציה לקבלת כל ההתערבויות
-export const getAllInterventions = functions.https.onRequest((req, res) => {
+// פונקציה לקבלת רשימת הטיפולים
+export const getInterventions = functions.https.onRequest((req, res) => {
   corsMiddleware(req, res, async () => {
-    const arr = [];
-    try {
-      const querySnapshot = await db.collection("interventions").get();
-      querySnapshot.forEach((doc) => {
-        arr.push(doc.data());
+    const uid = req.headers.uid;
+    const authHeader = req.headers.authorization;
+
+    if (!uid)
+      return res.status(403).send({
+        success: false,
+        message: "לא נשלח אימות uid בבקשה",
       });
-      res.status(200).send(arr);
+
+    if (!authHeader)
+      return res.status(403).send({
+        success: false,
+        message: "לא נשלח אימות Token בבקשה",
+      });
+
+    const idToken =
+      authHeader && authHeader.startsWith("Bearer ")
+        ? authHeader.split(" ")[1]
+        : null;
+
+    let user;
+    try {
+      user = await admin.auth().verifyIdToken(idToken);
     } catch (error) {
-      res.status(500).send("Error retrieving interventions data");
+      return res.status(401).send({
+        success: false,
+        message: "שגיאה באימות ה-ID Token",
+      });
+    }
+    const uidFromIdtoken = user.uid;
+    if (uid !== uidFromIdtoken) {
+      return res.status(403).send({
+        success: false,
+        message: "משתמש לא מאומת",
+      });
+    }
+
+    //  שליפת נתוני משתמש מ-firestore עפ"י uid
+    let userData = null;
+    try {
+      const q = db.collection("users").where("UID", "==", uid);
+      const querySnapshot = await q.get();
+      userData = querySnapshot.docs[0].data();
+    } catch (err) {
+      console.log("משתמש לא ידוע: ", err);
+    }
+
+    try {
+      const arr = [];
+      if (
+        userData.job_title === "מנהל ארגון" ||
+        userData.job_title === "יועץ"
+      ) {
+        const querySnapshot = await db.collection("interventions").get();
+        querySnapshot.forEach((doc) => {
+          arr.push(doc.data());
+        });
+        res.status(200).send({
+          success: true,
+          massage: arr,
+        });
+      } else if (userData.job_title === "מנהל ת\"ת") {
+        const studentIdArr = [];
+        const querySnapshot1 = await db
+          .collection("students")
+          .where("city_of_school", "==", userData.city)
+          .get();
+        querySnapshot1.forEach((doc) => {
+          studentIdArr.push(doc.data().student_id);
+        });
+        console.log("****student id arr: ", studentIdArr);
+
+        const querySnapshot2 = await db
+          .collection("interventions")
+          .where("student_id", "in", studentIdArr)
+          .get();
+        querySnapshot2.forEach((doc) => {
+          arr.push(doc.data());
+        });
+        res.status(200).send({
+          success: true,
+          massage: arr,
+        });
+      } else if (userData.job_title === "מטפל") {
+        const querySnapshot = await db
+          .collection("interventions")
+          .where("student_id", "in", userData.access_permissions.students)
+          .get();
+        querySnapshot.forEach((doc) => {
+          arr.push(doc.data());
+        });
+        res.status(200).send({
+          success: true,
+          massage: arr,
+        });
+      } else {
+        res.status(403).send({
+          success: false,
+          massage: "לא ניתן לקבל רשימת טיפולים, נא פנה למנהל המערכת",
+        });
+      }
+    } catch (error) {
+      res.status(500).send({
+        success: false,
+        massage: "שגיאה בזמן שליפת המידע",
+      });
     }
   });
 });
